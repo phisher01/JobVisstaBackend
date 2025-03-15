64c20447c080e293a7622b966a5411a8e847389d
Gagan Singh <gsgagan2003@gmail.com>
2025-03-15 18:15:06 +0530

    delivered

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://jobvisstafrontend.onrender.com/',
+  origin: 'https://jobvisstafrontend.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
